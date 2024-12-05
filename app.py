7052cb2e37ce8a96f152271d1f69e5f5f8fe5a6d
Adam Harris <adamharris95@gmail.com>
2024-12-05 19:54:24 +0000

    cleaned html

--- app.py
+++ app.py
@@ -58,27 +58,10 @@ Based on this conversation write a messages explaining this issue in clear langu
 Remember, dont make up any extra stuff and keep it polite.
 """
 
-# def generate_initial_response(user_input, pre_prompt):
-#     prompt = f'{pre_prompt}\n {user_input}'
-#     response = model.generate_content(prompt)
-#     return response.text
 
 
 app = Flask(__name__)
 app.secret_key = 'robosmile'  
-# def get_response(prompt):
-#     endpoint = aiplatform.Endpoint(endpoint_name="your_endpoint_name")
-#     response = endpoint.predict(instances=[{"text": prompt}])
-#     return response.predictions[0]["text"]
-
-# @app.route("/", methods=["GET", "POST"])
-# def index():
-#     if request.method == "POST":
-#         user_input = request.form["user_input"]
-#         response = generate_initial_response(user_input, pre_prompt)
-#         return render_template("index.html", response=response)
-#     else:
-#         return render_template("index.html")
 
 
 sess = {}
@@ -120,3 +103,20 @@ def summarise():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+# def get_response(prompt):
+#     endpoint = aiplatform.Endpoint(endpoint_name="your_endpoint_name")
+#     response = endpoint.predict(instances=[{"text": prompt}])
+#     return response.predictions[0]["text"]
+
+# @app.route("/", methods=["GET", "POST"])
+# def index():
+#     if request.method == "POST":
+#         user_input = request.form["user_input"]
+#         response = generate_initial_response(user_input, pre_prompt)
+#         return render_template("index.html", response=response)
+#     else:
+#         return render_template("index.html")
